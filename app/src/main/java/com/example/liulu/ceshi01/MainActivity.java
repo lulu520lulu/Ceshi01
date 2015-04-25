bf4d241de8ed51c53454a0aea9509eb819ff9c13
liulu <liuluzxc@126.com>
2015-04-25 15:16:22 +0800

    2

--- app/src/main/java/com/example/liulu/ceshi01/MainActivity.java
+++ app/src/main/java/com/example/liulu/ceshi01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.liulu.ceshi01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -10,6 +13,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_launcher);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IntentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
